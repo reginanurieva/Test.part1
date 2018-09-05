8a33034b28e511ee0cdda651da8ebf7db1162877
Regina Nurieva and Skye Nguyen <skye@>
2018-09-05 09:10:55 -0700

    add code to display the program in the console

--- Calendar/Models/LeapYear.cs
+++ Calendar/Models/LeapYear.cs
@@ -1,11 +1,34 @@
+using System;
+using Calendar;
+
 namespace Calendar
 {
   public class LeapYear
   {
     public bool IsLeapYear(int year)
     {
-      // logic will go here
-      return false;
+      if (year % 400 == 0)
+      {
+        return true;
+      }
+      else if (year % 100 == 0)
+      {
+        return false;
+      }
+      else
+      {
+        return year % 4 == 0;
+      }
     }
+  }
+}
+public class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Please enter the year.");
+    int enteredYear = int.Parse(Console.ReadLine());
+    LeapYear myLeapYear = new LeapYear();
+    Console.WriteLine(myLeapYear.IsLeapYear(enteredYear));
   }
 }
