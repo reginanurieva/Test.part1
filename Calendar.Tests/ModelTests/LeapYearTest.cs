94f74ccd33345f6c730676259ce51262f08d036c
Regina Nurieva and Skye Nguyen <skye@>
2018-09-05 08:50:50 -0700

    add lines of code to test the program

--- Calendar.Tests/ModelTests/LeapYearTest.cs
+++ Calendar.Tests/ModelTests/LeapYearTest.cs
@@ -9,7 +9,26 @@ namespace Calendar.Tests
     [TestMethod]
     public void IsLeapYear_NumberDivisibleByFour_True()
     {
-      // eventually your testing code will go here
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
+    }
+    [TestMethod]
+    public void IsLeapYear_NumberNotDivisibleByFour_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
+    }
+    [TestMethod]
+    public void IsLeapYear_MultiplesOfOneHundred_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
+    }
+    [TestMethod]
+    public void IsLeapYear_MultiplesOfFourHundred_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2000));
     }
   }
 }
